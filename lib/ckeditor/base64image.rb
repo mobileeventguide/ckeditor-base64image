b7ee096f73a451763ad551035566a4d2020ee7cb
Philipp Hansch <dev@phansch.net>
2014-11-24 11:45:17 +0100

    Cleanup, fixing path

--- lib/ckeditor/base64image.rb
+++ lib/ckeditor/base64image.rb
@@ -2,6 +2,9 @@ require "ckeditor/base64image/version"
 
 module Ckeditor
   module Base64image
-    # Your code goes here...
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
   end
 end
